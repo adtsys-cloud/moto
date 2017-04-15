ba1c919ee8247eb2234011adc262641b8ea2c803
GuyTempleton <Guy.Templeton@skyscanner.net>
2017-04-15 16:24:30 +0100

    Move agent_connected assignation

--- moto/ecs/models.py
+++ moto/ecs/models.py
@@ -262,6 +262,7 @@ class ContainerInstance(BaseObject):
 
     def __init__(self, ec2_instance_id):
         self.ec2_instance_id = ec2_instance_id
+        self.agent_connected = True
         self.status = 'ACTIVE'
         self.registered_resources = [
             {'doubleValue': 0.0,
@@ -286,7 +287,6 @@ class ContainerInstance(BaseObject):
              'name': 'PORTS_UDP',
              'stringSetValue': [],
              'type': 'STRINGSET'}]
-        self.agent_connected = True
         self.container_instance_arn = "arn:aws:ecs:us-east-1:012345678910:container-instance/{0}".format(
             str(uuid.uuid1()))
         self.pending_task_count = 0
