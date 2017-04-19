f1884987b4e272197568b98e942da9e3d2cffe06
Otoniel Isidoro <otoniel.isidoro@gmail.com>
2017-04-19 14:53:34 -0300

    add url from mock remote

--- moto/s3/urls.py
+++ moto/s3/urls.py
@@ -5,7 +5,8 @@ from .responses import S3ResponseInstance
 url_bases = [
     "https?://s3(.*).amazonaws.com",
     "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3(.*).amazonaws.com",
-    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.localhost"
+    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.localhost",
+    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.aws.mock.adtsys.com.br"
 ]
 
 
