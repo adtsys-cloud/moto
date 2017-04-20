61a1d4ae9a7d7a6b05d9be58c8f7d3298970851c
Otoniel Isidoro <otoniel.isidoro@gmail.com>
2017-04-20 17:14:53 -0300

    fix regex and template

--- moto/s3/urls.py
+++ moto/s3/urls.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 from .responses import S3ResponseInstance
 
 url_bases = [
-    "https?://s3(.*).amazonaws.com(/.*)?",
-    "https?://aws(.*)?.mock.adtsys.com.br(/.*)?",
-    "https?://localhost(/.*)?",
+    "https?://s3(.*).amazonaws.com(\/.*)?",
+    "https?://aws(.*)?.mock.adtsys.com.br(\/.*)?",
+    "https?://localhost(\/.*)?",
     "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3(.*).amazonaws.com",
     "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.localhost",
     "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.aws(.*)?.mock.adtsys.com.br"
